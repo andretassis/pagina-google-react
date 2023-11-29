212a112ec56caee97000356ff3747abed39e13fc
andretassis <tassisandre@gmail.com>
2023-11-29 17:37:48 -0300

    inicio da criacao do component de input para pesquisa

---
+++ src/components/InputSearch.js
@@ -0,0 +1,11 @@
+import style from './InputSearch.module.css'
+
+function InputSearch() {
+    return (
+        <>
+        <input type="text" className={style.input}></input>
+        </>
+    )
+}
+
+export default InputSearch
